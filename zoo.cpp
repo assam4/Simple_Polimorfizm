2104b62fc8ee4e56d9bc19e92afc9bffc01a17a8
assam4 <115891697+assam4@users.noreply.github.com>
2024-02-20 00:54:42 +0400

    Update zoo.cpp

--- zoo.cpp
+++ zoo.cpp
@@ -1,11 +1,12 @@
 #include "animal.h"
 #include <vector>
+#include <memory>
 
 inline void wait_for_a_symbhol() 
 {
     char symbhol;
     std::cin.clear();
-    std::cout << "\nenter any symbhol for Exit . ";
+    std::cout << "\n Enter any character for close. ";
     std::cin >> symbhol;
 }
 
@@ -13,16 +14,13 @@ int main ()
 {
     try
         {
-        std::vector<animal*> zoo ;
-         zoo.push_back(new lion("Simba"));
-         zoo.push_back(new fish("Nemo"));
-         zoo.push_back(new snake("Kan"));
-         zoo.push_back(new monkey("Abu"));
+        std::vector<std::unique_ptr<Animal>> zoo ;
+         zoo.push_back(new lion("Simba")); // insert lion
+         zoo.push_back(new fish("Nemo")); // insert fish
+         zoo.push_back(new snake("Kan")); // insert snake
+         zoo.push_back(new monkey("Abu")); // insert monkey
 
-         
-
-
-    for(auto animal : zoo) 
+    for(auto animal : zoo)  // calling all virtual functions
     {
         animal->eat();
         animal->move();
